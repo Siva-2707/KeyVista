da1b356e2d99ae6ee370547b410bb6bf153f277a
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2025-06-15 21:49:28 -0300

    bug(backend): NoSuchElementException handled

--- backend/src/main/java/com/siva/keyvista/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/siva/keyvista/exception/GlobalExceptionHandler.java
@@ -4,12 +4,16 @@ import com.siva.keyvista.util.FailureResponseEntity;
 import com.siva.keyvista.util.KeyVistaResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public KeyVistaResponse<?> handleGenericException(Exception ex) {
         return new KeyVistaResponse<>(
                 HttpStatus.INTERNAL_SERVER_ERROR,
@@ -19,6 +23,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public KeyVistaResponse<?> handleIllegalArgument(IllegalArgumentException ex) {
         return new KeyVistaResponse<>(
                 HttpStatus.BAD_REQUEST,
@@ -26,4 +31,16 @@ public class GlobalExceptionHandler {
                 new FailureResponseEntity(ex)
         );
     }
+
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public KeyVistaResponse<?> handleNoSuchElementException(NoSuchElementException ex) {
+        return new KeyVistaResponse<>(
+                HttpStatus.NOT_FOUND,
+                "NOT_FOUND",
+                new FailureResponseEntity(ex)
+        );
+    }
+
 }
