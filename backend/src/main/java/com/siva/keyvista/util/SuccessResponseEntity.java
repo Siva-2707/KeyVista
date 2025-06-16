a9d611f554ee212b4b58933531bf2946234909b6
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2025-06-15 21:57:01 -0300

    bug(backend): Added @Data for jackson to understand response

--- backend/src/main/java/com/siva/keyvista/util/SuccessResponseEntity.java
+++ backend/src/main/java/com/siva/keyvista/util/SuccessResponseEntity.java
@@ -1,8 +1,10 @@
 package com.siva.keyvista.util;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class SuccessResponseEntity<T> implements ResponseEntity {
     T data;
 }
