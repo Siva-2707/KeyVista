e074c7bec1f65547a7fffd82b26cb54bd1cde497
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2025-06-15 21:55:44 -0300

    bug(backend-auth): Rearranged role in SecurityFilterChain

--- backend/src/main/java/com/siva/keyvista/security/config/WebSecurityConfig.java
+++ backend/src/main/java/com/siva/keyvista/security/config/WebSecurityConfig.java
@@ -25,8 +25,8 @@ public class WebSecurityConfig {
                 .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                     .requestMatchers("/","/auth/**").permitAll()
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
                     .requestMatchers("/api/**").hasAnyRole( "USER","ADMIN")
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                     .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
