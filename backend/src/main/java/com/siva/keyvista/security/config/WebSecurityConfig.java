0bb2eea4c4492024c3f09f958dce323a60f7d426
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2025-06-26 19:09:08 -0300

    CORS

--- backend/src/main/java/com/siva/keyvista/security/config/WebSecurityConfig.java
+++ backend/src/main/java/com/siva/keyvista/security/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(frontEndUrl));
+        config.setAllowedOrigins(List.of(frontEndUrl,"*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
